bb7fb87763c0e80f132402da5d5c5a01b6a48177
Felipe <marketing@letscode.com.br>
2024-05-18 16:27:14 -0300

    add checkboxs

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import * as React from 'react';
-import { TextInput, Text,TouchableOpacity, View, Button,StyleSheet} from 'react-native';
+import { TextInput, Text, TouchableOpacity, View, Button, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {MaterialCommunityIcons} from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { createStackNavigator } from '@react-navigation/stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Card, Paragraph, Title } from 'react-native-paper';
+import { Card } from 'react-native-paper';
+import { CheckBox } from 'react-native-elements';
 
 const Tab = createBottomTabNavigator();
-
 const Stack = createStackNavigator();
 
-class Principal extends React.Component{
-  constructor(props){
+class Principal extends React.Component {
+  constructor(props) {
     super(props)
     this.state = {
       usuario: undefined,
@@ -20,66 +20,64 @@ class Principal extends React.Component{
     }
   }
 
-  render(){
-    return(
-    <View>
-      <Text>{"Usuário:"}</Text>
-      <TextInput onChangeText={(texto)=>this.setState({usuario: texto})}></TextInput>
-      <Text>{"Senha:"}</Text>
-      <TextInput onChangeText={(texto)=>this.setState({senha: texto})}></TextInput>
-      <Button title="Logar" onPress={()=>this.ler()}></Button>
-    </View>
+  render() {
+    return (
+      <View>
+        <Text>{"Usuário:"}</Text>
+        <TextInput onChangeText={(texto) => this.setState({ usuario: texto })}></TextInput>
+        <Text>{"Senha:"}</Text>
+        <TextInput onChangeText={(texto) => this.setState({ senha: texto })}></TextInput>
+        <Button title="Logar" onPress={() => this.ler()}></Button>
+      </View>
     )
   }
 
-  async ler(){
-    try{
+  async ler() {
+    try {
       let senha = await AsyncStorage.getItem(this.state.usuario);
-      if(senha != null){
-        if(senha == this.state.senha){
+      if (senha != null) {
+        if (senha == this.state.senha) {
           //alert("Logado!!!");
           this.props.navigation.navigate('Tela3')
-        }else{
+        } else {
           alert("Senha Incorreta!");
         }
-      }else{
+      } else {
         alert("Usuário não foi encontrado!");
       }
-    }catch(erro){
+    } catch (erro) {
       console.log(erro);
     }
   }
 }
 
-class Cadastro extends React.Component{
-  constructor(props){
+class Cadastro extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       user: undefined,
       password: undefined,
-
     }
-
   }
 
-  async gravar(){
-    try{
+  async gravar() {
+    try {
       await AsyncStorage.setItem(this.state.user, this.state.password);
       alert("Salvo com sucesso!!!")
-    }catch(erro){
+    } catch (erro) {
       alert("Erro!")
     }
   }
 
-  render(){
-    return(
-    <View>
-      <Text>{"Cadastrar Usuário:"}</Text>
-      <TextInput onChangeText={(texto)=>this.setState({user: texto})}></TextInput>
-      <Text>{"Cadastrar Senha:"}</Text>
-      <TextInput onChangeText={(texto)=>this.setState({password: texto})}></TextInput>
-      <Button title="Cadastrar" onPress={()=>this.gravar()}/>
-    </View>
+  render() {
+    return (
+      <View>
+        <Text>{"Cadastrar Usuário:"}</Text>
+        <TextInput onChangeText={(texto) => this.setState({ user: texto })}></TextInput>
+        <Text>{"Cadastrar Senha:"}</Text>
+        <TextInput onChangeText={(texto) => this.setState({ password: texto })}></TextInput>
+        <Button title="Cadastrar" onPress={() => this.gravar()} />
+      </View>
     )
   }
 }
@@ -87,15 +85,15 @@ class Cadastro extends React.Component{
 class Nav2 extends React.Component {
   render() {
     return (
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={Principal} />
-          <Stack.Screen name="Tela3" component={Tela3}/>
-          <Stack.Screen name="Tela4" component={Tela4}/>
-          <Stack.Screen name="Tela5" component={Tela5}/>
-          <Stack.Screen name="Tela6" component={Tela6}/>
-          <Stack.Screen name="Tela7" component={Tela7}/>
-          <Stack.Screen name="Tela8" component={Tela8}/>
-       </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Principal} />
+        <Stack.Screen name="Tela3" component={Tela3} />
+        <Stack.Screen name="Tela4" component={Tela4} />
+        <Stack.Screen name="Tela5" component={Tela5} />
+        <Stack.Screen name="Tela6" component={Tela6} />
+        <Stack.Screen name="Tela7" component={Tela7} />
+        <Stack.Screen name="Tela8" component={Tela8} />
+      </Stack.Navigator>
     );
   }
 }
@@ -129,114 +127,259 @@ class Tela3 extends React.Component {
     );
   }
 }
-class Tela4 extends React.Component {
-    render() {
-    return (
-      <View style={{flex: 1, justifyContent: 'center'}}>
-         <Card>
-          <Card.Title title="Relatoório A"/>
-          <Card.Content>
-            //checkboxs aqui
-          </Card.Content>
-        </Card>
-      </View>
-    );
-  }
-}
-class Tela5 extends React.Component {
-    render() {
-    return (
-      <View style={{flex: 1, justifyContent: 'center'}}>
-         <Card>
-          <Card.Title title="Relatoório B"/>
-          <Card.Content>
-            //checkboxs aqui
-          </Card.Content>
-        </Card>
-      </View>
-    );
-  }
-}
-class Tela6 extends React.Component {
-    render() {
-    return (
-      <View style={{flex: 1, justifyContent: 'center'}}>
-         <Card>
-          <Card.Title title="Relatoório C"/>
-          <Card.Content>
-            //checkboxs aqui
-          </Card.Content>
-        </Card>
-      </View>
-    );
-  }
-}
-class Tela7 extends React.Component {
-    render() {
-    return (
-      <View style={{flex: 1, justifyContent: 'center'}}>
-         <Card>
-          <Card.Title title="Relatoório D"/>
-          <Card.Content>
-            //checkboxs aqui
-          </Card.Content>
-        </Card>
-      </View>
-    );
-  }
-}
-class Tela8 extends React.Component {
-    render() {
-    return (
-      <View style={{flex: 1, justifyContent: 'center'}}>
-         <Card>
-          <Card.Title title="Relatoório E"/>
-          <Card.Content>
-            //checkboxs aqui
-          </Card.Content>
-        </Card>
-      </View>
-    );
-  }
-}
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    paddingHorizontal:10,
-  },
-  button:{
-    alignItems:'center',
-    backgroundColor:'#DDDDDD',
-    marginBottom:'10px',
-    padding:10,
-    borderColor:'#000000',
-  },
-  countContainer:{
-    alignItems:'center',
-    padding:10,
-  }
 
-})
-class App extends React.Component {
+class Tela4 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      check1: false,
+      check2: false,
+      check3: false,
+    };
+  }
 
   render() {
-    return(
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Login" component={Nav2} 
-          options={{
-            tabBarIcon: ({color, size}) => (<MaterialCommunityIcons name="home-account" color={color} size={size}/>)
-            , headerShown: false
-          }}
-        />
-        <Tab.Screen name="Criar Usuário" component={Cadastro}
-          options={{
-            tabBarIcon: ({color, size}) => (<MaterialCommunityIcons name="account-details" color={color} size={size}/>)
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Card>
+          <Card.Title title="Relatório A" />
+          <Card.Content>
+            <CheckBox
+              title="Opção 1"
+              checked={this.state.check1}
+              onPress={() => this.setState({ check1: !this.state.check1 })}
+            />
+            <CheckBox
+              title="Opção 2"
+              checked={this.state.check2}
+              onPress={() => this.setState({ check2: !this.state.check2 })}
+            />
+            <CheckBox
+              title="Opção 3"
+              checked={this.state.check3}
+              onPress={() => this.setState({ check3: !this.state.check3 })}
+            />
+          </Card.Content>
+        </Card>
+      </View>
+    );
+  }
+}
+
+class Tela5 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      check1: false,
+      check2: false,
+      check3: false,
+    };
+  }
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Card>
+          <Card.Title title="Relatório B" />
+          <Card.Content>
+            <CheckBox
+              title="Opção 1"
+              checked={this.state.check1}
+              onPress={() => this.setState({ check1: !this.state.check1 })}
+            />
+            <CheckBox
+              title="Opção 2"
+              checked={this.state.check2}
+              onPress={() => this.setState({ check2: !this.state.check2 })}
+            />
+            <CheckBox
+              title="Opção 3"
+              checked={this.state.check3}
+              onPress={() => this.setState({ check3: !this.state.check3 })}
+            />
+          </Card.Content>
+        </Card>
+      </View>
+    );
+  }
+}
+
+class Tela6 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      check1: false,
+      check2: false,
+      check3: false,
+    };
+  }
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Card>
+          <Card.Title title="Relatório C" />
+          <Card.Content>
+            <CheckBox
+              title="Opção 1"
+              checked={this.state.check1}
+              onPress={() => this.setState({ check1: !this.state.check1 })}
+            />
+            <CheckBox
+              title="Opção 2"
+              checked={this.state.check2}
+              onPress={() => this.setState({ check2: !this.state.check2 })}
+            />
+            <CheckBox
+              title="Opção 3"
+              checked={this.state.check3}
+              onPress={() => this.setState({ check3: !this.state.check3 })}
+            />
+          </Card.Content>
+        </Card>
+      </View>
+    );
+  }
+}
+
+class Tela7 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      check1: false,
+      check2: false,
+      check3: false,
+    };
+  }
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Card>
+          <Card.Title title="Relatório D" />
+          <Card.Content>
+            <CheckBox
+              title="Opção 1"
+              checked={this.state.check1}
+              onPress={() => this.setState({ check1: !this.state.check1 })}
+            />
+            <CheckBox
+              title="Opção 2"
+              checked={this.state.check2}
+              onPress={() => this.setState({ check2: !this.state.check2 })}
+            />
+            <CheckBox
+              title="Opção 3"
+              checked={this.state.check3}
+              onPress={() => this.setState({ check3: !this.state.check3 })}
+            />
+          </Card.Content>
+        </Card>
+      </View>
+    );
+  }
+}
+
+class Tela8 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      check1: false,
+      check2: false,
+      check3: false,
+    };
+  }
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Card>
+          <Card.Title title="Relatório E" />
+          <Card.Content>
+            <CheckBox
+              title="Opção 1"
+              checked={this.state.check1}
+              onPress={() => this.setState({ check1: !this.state.check1 })}
+            />
+            <CheckBox
+              title="Opção 2"
+              checked={this.state.check2}
+              onPress={() => this.setState({ check2: !this.state.check2 })}
+            />
+            <CheckBox
+              title="Opção 3"
+              checked={this.state.check3}
+              onPress={() => this.setState({ check3: !this.state.check3 })}
+            />
+          </Card.Content>
+        </Card>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    marginBottom: 10,
+    padding: 10,
+    borderColor: '#000000',
+  },
+  countContainer: {
+    alignItems: 'center',
+    padding: 10,
+  }
+})
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+    };
+  }
+
+  handleLogin = () => {
+    this.setState({ loggedIn: true });
+  }
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator>
+          {!this.state.loggedIn ? (
+            <>
+              <Tab.Screen name="Login" component={Nav2}
+                options={{
+                  tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="home-account" color={color} size={size} />)
+                  , headerShown: false
+                }}
+                initialParams={{ onLogin: this.handleLogin }}
+              />
+              <Tab.Screen name="Criar Usuário" component={Cadastro}
+                options={{
+                  tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="account-details" color={color} size={size} />)
+                }}
+              />
+            </>
+          ) : (
+            <>
+              <Tab.Screen name="Meus Relatórios" component={Tela3}
+                options={{
+                  tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="file-document" color={color} size={size} />)
+                }}
+              />
+              <Tab.Screen name="Perfil" component={Cadastro}
+                options={{
+                  tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="account" color={color} size={size} />)
+                }}
+              />
+            </>
+          )}
+        </Tab.Navigator>
+      </NavigationContainer>
     )
   }
 }
